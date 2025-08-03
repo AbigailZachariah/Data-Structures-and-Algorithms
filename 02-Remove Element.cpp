8f2624a78c072a131e17c6fbd7ce7a5a07f670b2
AbigailZachariah <abigail2005zachariah@gmail.com>
2025-08-03 11:39:18 +0530

    Removing Element-27 LeetCode
    This code accepts a vector from the user and returns the number of elements which are not equal to the value entered .
    The original array is changed in such a way that the elements not equal to the value are placed at the beginning of the array.

---
+++ 02-Remove Element.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int k=0; //The number of the elements not egual to val
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]!=val){
+                nums[k]=nums[i];
+                k+=1;
+            }
+        } 
+        return k;
+    }
+};
+int main(){
+    Solution sol; //Object creation
+    vector<int>nums;
+    int n;
+    cout<<"Enter number of elements: ";
+    cin>>n;
+    for(int i=0;i<n;i++){
+        int number;
+        cout<<"Enter the elment: ";
+        cin>>number;
+        nums.push_back(number);
+    }
+    int val;
+    cout<<"Enter the value:";
+    cin>>val;
+    int result=sol.removeElement(nums,val);
+    cout<<"The number of elements which are not equal to val:"<<result;
+    return 0;
+}

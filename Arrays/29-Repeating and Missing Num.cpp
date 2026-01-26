a9e72a712cfee040c50273e0523056c9f3c2c14f
Abigail Zachariah <abigail2005zachariah@gmail.com>
2026-01-27 01:40:51 +0530

    Repeat Missing

---
+++ Arrays/29-Repeating and Missing Num.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> RepeatMissing(vector<int> &nums)
+{
+    long long n = nums.size();
+    long long SN = (n * (n + 1)) / 2;
+    long long S2N = (n * (n + 1) * (2 * n + 1)) / 6;
+    long long S = 0, S2 = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        S += nums[i];
+        S2 += (long long)nums[i] * (long long)nums[i];
+    }
+    long long val1 = S - SN;
+    long long val2 = S2 - S2N;
+
+    val2 = val2 / val1;
+    long long x = (val1 + val2) / 2;
+    long long y = x - val1;
+
+    return {(int)x, (int)y};
+}
+
+int main()
+{
+    vector<int> arr;
+    int num;
+    cout << "Enter the number of elements:";
+    cin >> num;
+
+    cout << "Enter the elements:";
+    cout << endl;
+    int ele;
+    for (int i = 0; i < num; i++)
+    {
+        cin >> ele;
+        arr.push_back(ele);
+    }
+    vector<int> result = RepeatMissing(arr);
+    cout << "Repeating:" << result[0] << " Missing:" << result[1];
+    return 0;
+}

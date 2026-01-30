27733c45bc0285673cb55972f0717551f34c8675
Abigail Zachariah <abigail2005zachariah@gmail.com>
2026-01-30 18:16:41 +0530

    Count occurences of an element

---
+++ Binary Search/05-Count Occurences of number.cpp
@@ -0,0 +1,89 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int lowerBound(vector<int> nums, int n, int x)
+{
+    int low = 0;
+    int high = n - 1;
+    int ans = n;
+    while (low <= high)
+    {
+        int mid = (low + high) / 2;
+        if (nums[mid] >= x)
+        {
+            ans = mid;
+            high = mid - 1;
+        }
+        else
+        {
+            low = mid + 1;
+        }
+    }
+    return ans;
+}
+int upperBound(vector<int> nums, int n, int x)
+{
+    int low = 0;
+    int high = n - 1;
+    int ans = n;
+    while (low <= high)
+    {
+        int mid = (low + high) / 2;
+        if (nums[mid] > x)
+        {
+            ans = mid;
+            high = mid - 1;
+        }
+        else
+        {
+            low = mid + 1;
+        }
+    }
+    return ans;
+}
+vector<int> searchRange(vector<int> &nums, int target)
+{
+    int n = nums.size();
+    int first = lowerBound(nums, n, target);
+    if (first == n || nums[first] != target)
+    {
+        return {-1, -1};
+    }
+    else
+    {
+        return {first, upperBound(nums, n, target) - 1};
+    }
+}
+int countOccurrences(vector<int> &arr, int target)
+{
+    vector<int> result = searchRange(arr, target);
+    if (result[0] == -1)
+        return 0;
+    else
+    {
+        return result[1] - result[0] + 1;
+    }
+}
+
+int main()
+{
+    vector<int> arr;
+    int num;
+    int target;
+    cout << "Enter the number of elements:";
+    cin >> num;
+
+    cout << "Enter the elements:";
+    cout << endl;
+    int ele;
+    for (int i = 0; i < num; i++)
+    {
+        cin >> ele;
+        arr.push_back(ele);
+    }
+    cout<<"Enter the target value:";
+    cin>>target;
+    
+    cout << "Number of Occurences:"<<countOccurrences(arr,target);
+    return 0;
+}

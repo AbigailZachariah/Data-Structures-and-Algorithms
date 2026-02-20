fa87d42fee455397f5098d17bfffd9798c1ddc29
Abigail Zachariah <abigail2005zachariah@gmail.com>
2026-02-20 22:37:26 +0530

    Maximum Nesting Depth

---
+++ Strings/08-Max nesting depth.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int maxDepth(string s){
+    int cnt=0,max_depth=0;
+    for(char ch:s){
+        if(ch=='(') cnt++;
+        else if(ch==')') cnt--;
+        max_depth=max(max_depth,cnt);
+    }
+    return max_depth;
+}
+
+int main(){
+    string str;
+    cout<<"Enter the string:";
+    cin>>str;
+
+    cout<<"Mximum Nesting Depth:"<<maxDepth(str);
+    return 0;
+}

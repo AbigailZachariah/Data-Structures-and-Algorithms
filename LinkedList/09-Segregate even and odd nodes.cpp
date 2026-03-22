a94cab0552a11ce22b3ed1c2580cac55341a136c
Abigail Zachariah <abigail2005zachariah@gmail.com>
2026-03-22 16:32:33 +0530

    Segregate odd and even

---
+++ LinkedList/09-Segregate even and odd nodes.cpp
@@ -0,0 +1,78 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node{
+public:
+    int data;
+    Node *next;
+
+public:
+    Node(int data1, Node *next1){
+        data = data1;
+        next = next1;
+    }
+
+public:
+    Node(int data1){
+        data = data1;
+        next = nullptr;
+    }
+};
+
+Node *convertArr2LL(vector<int> &arr){
+    Node *head = new Node(arr[0]);
+    Node *mover = head;
+
+    for (int i = 1; i < arr.size(); i++){
+        Node *temp = new Node(arr[i]);
+        mover->next = temp;
+        mover = temp;
+    }
+    return head;
+}
+
+void print(Node* head){
+    Node* temp=head;
+    while(temp!=NULL){
+        cout<<temp->data<<" ";
+        temp=temp->next;
+    }
+    cout<<endl;
+}
+
+Node* oddEvenList(Node* head) {
+    if(head==NULL || head->next==NULL) return head;
+    Node*odd=head;
+    Node*even=head->next;
+    Node*evenHead=head->next;
+    while(even!=NULL && even->next!=NULL){
+         
+        odd->next=odd->next->next;
+        even->next=even->next->next;
+
+        odd=odd->next;
+        even=even->next;
+    }
+    odd->next=evenHead;
+    return head;
+}
+
+int main(){
+    vector<int>nums;
+    int n;
+    cout<<"Enter number of elements: ";
+    cin>>n;
+
+    int number;
+    cout<<"Enter the element: ";
+    for(int i=0;i<n;i++){
+        cin>>number;
+        nums.push_back(number);
+    }
+    Node*head=convertArr2LL(nums);
+    Node* segregateOddEven=oddEvenList(head);
+    
+    print(segregateOddEven);
+    
+    return 0;
+}

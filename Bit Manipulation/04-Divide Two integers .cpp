0c60330e89a21f98cbd6f5c7ba740b68e7b05b83
Abigail Zachariah <abigail2005zachariah@gmail.com>
2026-06-08 12:27:17 +0300

    Divide without * or /

---
+++ Bit Manipulation/04-Divide Two integers .cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int divide(int dividend, int divisor) {
+
+    if(dividend==divisor)return 1;
+
+    bool sign=true;
+    if(dividend>=0 && divisor<0) sign =false;
+    else if(dividend<=0 && divisor>0) sign =false;
+
+    long n=abs((long)dividend);
+    long d=abs((long)divisor);
+
+    long quotient=0;
+
+    while(n>=d){
+        int cnt=0;
+        while(n>=(d<<(cnt+1)))
+            cnt++;
+
+        quotient+=1<<cnt;
+        n-=(d<<cnt);
+    }
+
+    if(quotient==1<<31 && sign) return INT_MAX;
+    if(quotient==1<<31 && !sign) return INT_MIN;
+
+    return sign? quotient : -quotient;
+}
+
+int main(){
+    int n;
+    cout<<"Enter dividend:";
+    cin>>n;
+
+    int n2;
+    cout<<"Enter divisor:";
+    cin>>n2;
+
+    cout<<divide(n,n2);
+    
+    return 0;
+}

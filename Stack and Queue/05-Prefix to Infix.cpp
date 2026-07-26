fd0758ddf7531ab0242c738ac43ce5aabe4917c4
Abigail Zachariah <abigail2005zachariah@gmail.com>
2026-07-26 20:36:04 +0530

    Prefix to Infix

---
+++ Stack and Queue/05-Prefix to Infix.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string prefixToInfix(string s){
+    int i=s.length()-1;
+    stack<string>st;
+
+    while(i>=0){
+        if((s[i]>='A' && s[i]<='Z') || (s[i]>='a' && s[i]<='z') || (s[i]>='0' && s[i]<='9')){
+            st.push(string(1,s[i]));
+        }
+        else{
+            string t1=st.top();
+            st.pop();
+            string t2=st.top();
+            st.pop();
+
+            string added='('+t1+s[i]+t2+')';
+            st.push(added);
+        }
+        i--;
+    }
+    return st.top();
+}
+
+int main(){
+    string s;
+    cin>>s;
+    cout<<prefixToInfix(s);
+    return 0;
+}
